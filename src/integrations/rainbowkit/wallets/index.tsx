f36628b7a7368a535d03cc9f2d85aded1fc58766
Mohak <mohaknainmalhotra@gmail.com>
2024-01-07 17:30:27 +1100

    Exports update

--- src/integrations/rainbowkit/wallets/index.tsx
+++ src/integrations/rainbowkit/wallets/index.tsx
@@ -1,6 +1,3 @@
 export { googleWallet } from "./googleWallet.js";
 export { twitterWallet } from "./twitterWallet.js";
 export { twitchWallet } from "./twitchWallet.js";
-export { facebookWallet } from "./facebookWallet.js";
-export { githubWallet } from "./githubWallet.js";
-export { discordWallet } from "./discordWallet.js";
